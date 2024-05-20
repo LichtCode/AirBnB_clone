8eb81ef21f9005740240edcff629f7246b52f57d
LichtCode <prinzdarey@gmail.com>
2024-05-20 20:30:04 +0100

    made more changes

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel():
     """The BaseModel class from which future classes will be derived"""
     def __init__(self):
         """Initialization of the base model"""
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
 
