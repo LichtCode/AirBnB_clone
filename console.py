71d3b87b2d88e69511211595a38fa096b4eb29ff
LichtCode <prinzdarey@gmail.com>
2024-05-21 11:13:55 +0100

    made more changes

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ console
 """
 import cmd
 import re
-import shlex
+from shlex import split
 import ast
 from models import storage
 from models.base_model import BaseModel
@@ -15,6 +15,128 @@ class HBNBCommand(cmd.Cmd):
     Custom console class
     """
     prompt = "(hbnb)"
+
+    valid_cls = ["BaseModel", "User",
+                 "Place", "State", "City",
+                 "Amenity", "Review"]
+
+    def do_create(self, arg):
+        """Creates a new instance of a class"""
+        arg_list = split(arg)
+
+        if len(arg_list) == 0:
+            print("** class name missing **")
+        elif arg_list[0] not in self.valid_cls:
+            print("** class doesn't exist **")
+        else:
+            new_instance = eval(arg_list[0] + "()")
+            storage.save()
+            print(new_instance.id)
+
+    def do_show(self, arg):
+        """Prints a string representation of an instance.
+
+        Args:
+            arg(arg): to enter with command <class name> <id>
+            Example: 'show User 1234-1234-1234'
+
+        """
+        arg_list = split(arg)
+
+        if len(arg_list) == 0:
+            print("** class name missing **")
+        elif arg_list[0] not in self.valid_cls:
+            print("** class doesn't exist **")
+        elif len(arg_list) < 2:
+            print("** instance id missing **")
+        else:
+            obj_list = storage.all()
+            obj_cls = arg_list[0]
+            obj_id = arg_list[1]
+            key = "{}.{}".format(obj_cls, obj_id)
+
+            if key in obj_list:
+                print(obj_list[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class and id"""
+        arg_list = split(arg)
+
+        if len(arg_list) == 0:
+            print("** class name missing **")
+        elif arg_list[0] not in self.valid_cls:
+            print("** class doesn't exist **")
+        elif len(arg_list) < 2:
+            print("** instance id missing **")
+        else:
+            obj_list = storage.all()
+            obj_cls = arg_list[0]
+            obj_id = arg_list[1]
+            key = "{}.{}".format(obj_cls, obj_id)
+
+            if key in obj_list:
+                del obj_list[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """Prints string representations of instances"""
+        arg_list = split(arg)
+        obj_list = storage.all()
+        arg_obj = []
+        if len(arg_list) > 0:
+            if arg_list[0] not in self.valid_cls:
+                print("** class doesn't exist **")
+            else:
+                for key in obj_list.keys():
+                    key_cls = key.split(".")[0]
+                    if key_cls == arg_list[0]:
+                        arg_obj.append(str(obj_list[key]))
+
+        else:
+            for value in obj_list.values():
+                arg_obj.append(str(value))
+
+        if len(arg_obj) > 0:
+            print(arg_obj)
+
+    def do_update(self, arg):
+        """Update an instance based on the class name,
+            id, attribute & value
+        """
+        arg_list = split(arg)
+
+        if len(arg_list) == 0:
+            print("** class name missing **")
+        elif arg_list[0] not in self.valid_cls:
+            print("** class doesn't exist **")
+        elif len(arg_list) < 2:
+            print("** instance id missing **")
+        else:
+            obj_list = storage.all()
+            obj_cls = arg_list[0]
+            obj_id = arg_list[1]
+            key = "{}.{}".format(obj_cls, obj_id)
+
+            if key not in obj_list:
+                print("** no instance found **")
+            elif len(arg_list) < 3:
+                print("** attribute name missing **")
+            elif len(arg_list) < 4:
+                print("** value missing **")
+            else:
+                obj = obj_list[key]
+                attr_name = arg_list[2]
+                attr_value = arg_list[3]
+                try:
+                    attr_value = eval(attr_value)
+                except Exception:
+                    pass
+                setattr(obj, attr_name, attr_value)
+                obj.save()
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
